e614df7347237faa0af11268c9e232687d937aeb
Zach Izzard <zachizzard@gmail.com>
2023-02-26 23:48:55 -0500

    Adding initial work

---
+++ src/ImageData.js
@@ -0,0 +1,30 @@
+import img001 from "./images/001.png";
+import img002 from "./images/002.png";
+import img003 from "./images/003.png";
+import img004 from "./images/004.png";
+import img005 from "./images/005.png";
+import img006 from "./images/006.png";
+import img007 from "./images/007.png";
+import img008 from "./images/008.png";
+import img009 from "./images/009.png";
+import img010 from "./images/010.png";
+import img011 from "./images/011.png";
+import img012 from "./images/012.png";
+import img013 from "./images/013.png";
+import img014 from "./images/014.png";
+import img015 from "./images/015.png";
+import img016 from "./images/016.png";
+import img017 from "./images/017.png";
+import img018 from "./images/018.png";
+import img019 from "./images/019.png";
+import img020 from "./images/020.png";
+import img021 from "./images/021.png";
+import img022 from "./images/022.png";
+
+const ImageData = [img001, img002, img003, img004, img005,
+                   img006, img007, img008, img009, img010,
+                   img011, img012, img013, img014, img015,
+                   img016, img017, img018, img019, img020,
+                   img021, img022]
+
+export default ImageData;
---
+++ src/Playlist.js
@@ -0,0 +1,41 @@
+import './App.css';
+import './Playlist.css';
+import React, { useState, useEffect } from 'react';
+
+function Playlist({data, delay}) {
+  const [display, setDisplay] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDisplay(true);
+    }, delay);
+    return () => clearTimeout(timer);
+  }, [delay]);
+
+  function goToPlaylist(){
+    console.log(data.url);
+    // window.location = data.url;
+  }
+  
+
+  return (
+    <>
+    {
+      display ? (
+        <a className={"playlist fade-in"} href={data.url} target="_blank" rel="noreferrer">
+          <div className="small-border">
+            <img className="playlist-image" src={data.img} />
+          </div>
+          <div className="playlist-title">
+            <div className="small-text-box">{data.title}</div>
+          </div>
+        </a>
+      ) : (
+         <a className={"playlist"}></a>
+      )
+    }
+    </>
+  );
+}
+
+export default Playlist;
